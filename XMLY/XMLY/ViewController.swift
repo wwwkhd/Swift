47b0eb7f7b7d673d25f6373432fe21fce3ab7cfc
wwwkhd <wwwkhd@163.com>
2017-03-18 22:14:27 +0800

    Add cocoapods

---
+++ XMLY/XMLY/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XMLY
+//
+//  Created by wangwei on 2017/3/18.
+//  Copyright © 2017年 wangwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
